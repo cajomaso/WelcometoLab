4e6096eee31b7b2d25a801854efa1a0b1ede3cf6
cajomaso <casoeder@gmail.com>
2026-07-06 23:34:43 +0200

    upd include background picture first demo of a demo

--- main.py
+++ main.py
@@ -12,14 +12,15 @@ screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Digi Lab')
 
 
-bildneutral = pygame.image.load("Arsenneutral.png").convert_alpha()
-img2 = pygame.image.load("Arsenhappy.png").convert_alpha()
-img3 = pygame.image.load("Arsensad.png").convert_alpha()
-img4 = pygame.image.load("Arsenspeaking.png").convert_alpha()
+bildneutral = pygame.image.load("media/character/arsen/Arsen neutral.png").convert_alpha()
+img2 = pygame.image.load("media/character/arsen/Arsen happy.png").convert_alpha()
+img3 = pygame.image.load("media/character/arsen/Arsen sad.png").convert_alpha()
+img4 = pygame.image.load("media/character/arsen/Arsen speaking.png").convert_alpha()
+background = pygame.image.load("media/background/digilabbackground.jpg").convert_alpha()
     #img = pygame.transform.scale(img, (70, 100))
 
 
-textbox2 = TextBox.TextBox((100, 500, 1000, 200))#x, y, width height
+textbox2 = textbox.TextBox((100, 500, 1000, 200))#x, y, width height
 
 textbox2.text = "Welcome to Digi Lab."
 
@@ -43,12 +44,11 @@ while running:
             if textbox2.active:
                 textbox2.text_input(event)
 
-        screen.blit(bildneutral, (0, 0))
 
         #textbox1.test_collide(event)
         textbox2.test_collide(event)
-
-
+    screen.blit(background, (0, 0))
+    screen.blit(bildneutral, (0, 0))
     #textbox1.draw(textbox1.text, screen)
     textbox2.draw(textbox2.text, screen)
     pygame.display.flip()
